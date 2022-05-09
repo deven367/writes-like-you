13af1db6500984959b57ec56fd7bf431eb8cda0c
Deven <masterdeven@gmail.com>
2022-05-08 22:30:55 -0400

    remove payload from api args

--- notion.py
+++ notion.py
@@ -2,18 +2,19 @@ from wsgiref import headers
 import requests
 import pandas as pd
 from constants import NOTION_DATABASE_ID, NOTION_TOKEN
-
+from tqdm import tqdm
 def query_db(db_id):
     url = f"https://api.notion.com/v1/databases/{db_id}/query"
-    response = requests.request("POST", url, headers=headers)
+    response = requests.request("POST", url, json=payload, headers=headers)
     return response.json()
 
 def query_page(page_id):
     url = f"https://api.notion.com/v1/pages/{page_id}"
-    response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", url, json=payload, headers=headers)
     return response.json()
 
 def query_block(block_id):
+    payload = {}
     url = f"https://api.notion.com/v1/blocks/{block_id}/children"
 
     response = requests.request("GET", url, headers=headers)
@@ -21,17 +22,24 @@ def query_block(block_id):
     return response.json()
 
 def process_block(block):
+    # print(block)
     l = len(block['results'])
     # assert l > 0, f"Block has no children"
     if l == 0:
         return ""
 
     else:
-
+        r = ""
+        # print(block)
         for result in block['results']:
-            r = result['paragraph']['rich_text'][0]['plain_text']
+            if 'paragraph' in result.keys():
+                # return result['properties']['title']
+                try:
+                    r += result['paragraph']['rich_text'][0]['plain_text']
+                except:
+                    continue
         # print(result['paragraph']['rich_text'][0]['plain_text'])
-            return r
+        return r
     # print(block)
 
 headers = {
@@ -41,17 +49,23 @@ headers = {
     "Content-Type": "application/json"
 }
 
+payload = {}
+# payload = {
+#     "page_size": 1,
+#     "start_cursor": "cursor",
+# }
+
 def main():
     ids, dates = [], []
     data = query_db(NOTION_DATABASE_ID)
-    # print(type(data))
+    # print(data)
     # print(len(data['results']))
     # print(data['results'][0])
     for result in data['results']:
         ids.append(result['id'])
         dates.append(result['created_time'])
 
-    print(len(data['results']))
+    # print(len(data['results']))
 
     while data['has_more']:
         payload['start_cursor'] = data['next_cursor']
@@ -62,19 +76,23 @@ def main():
 
     df = pd.DataFrame(data={"id": ids, "ts": dates})
     df['ts'] = pd.to_datetime(df['ts'])
-    # print(df.info())
     df['date'] = df['ts'].dt.strftime('%Y-%m-%d')
-    # print(df.head())
     df.sort_values(by=['date'], inplace=True)
     df.reset_index(drop=True, inplace=True)
-    # print(df.head())
 
-    pid = df.loc[0, 'id']
-    block = query_block(pid)
+    # pid = df.loc[0, 'id']
+    # pid = '7edba87fc83d4064a7a8308ea0c788bf'
+    # block = query_block(pid)
     # print(block)
     # process_block(block)
+    # texts = []
+    # for i in tqdm(range(len(df))):
+    #     pid = df.loc[i, 'id']
+    #     block = query_block(pid)
+    #     text = process_block(block)
+    #     texts.append(text)
+
     df['text'] = df.apply(lambda x: process_block(query_block(x['id'])), axis=1)
-    print(df)
     df.to_csv("data.csv", index=False)
 
 
